abe9705f42a61d43247cca84bad914cc98950246
Daniel Roy Greenfeld <danny@eventbrite.com>
2015-10-17 13:29:44 -0700

    Fallback to JSON

--- cookiecutter/config.py
+++ cookiecutter/config.py
@@ -10,14 +10,10 @@ Global configuration handling
 
 from __future__ import unicode_literals
 import copy
+import io
+import json
 import logging
 import os
-import io
-
-try:
-    import ruamel.yaml as yaml
-except ImportError:
-    import yaml
 
 from .exceptions import ConfigDoesNotExistException
 from .exceptions import InvalidConfiguration
@@ -32,6 +28,20 @@ DEFAULT_CONFIG = {
 }
 
 
+def get_yaml_parser():
+
+    # Try to get the best YAML library
+    use_yaml = True
+    try:
+        import yaml
+    except ImportError:
+        # Fall back to ruamel.yaml if it exists
+        try:
+            import ruamel.yaml as yaml
+        except ImportError:
+            raise
+
+
 def get_config(config_path):
     """
     Retrieve the config from the specified path, returning it as a config dict.
@@ -40,19 +50,28 @@ def get_config(config_path):
     if not os.path.exists(config_path):
         raise ConfigDoesNotExistException
 
+    try:
+        yaml = get_yaml_parser()
+    except ImportError:
+        yaml = None
+
     logger.debug('config_path is {0}'.format(config_path))
+
     with io.open(config_path, encoding='utf-8') as file_handle:
-        try:
-            yaml_dict = yaml.safe_load(file_handle)
-        except yaml.scanner.ScannerError as e:
-            raise InvalidConfiguration(
-                '{0} is not a valid YAML file: line {1}: {2}'.format(
-                    config_path,
-                    e.problem_mark.line,
-                    e.problem))
+        if yaml:
+            try:
+                config_dict = yaml.safe_load(file_handle)
+            except yaml.scanner.ScannerError as e:
+                raise InvalidConfiguration(
+                    '{0} is not a valid YAML file: line {1}: {2}'.format(
+                        config_path,
+                        e.problem_mark.line,
+                        e.problem))
+        else:  # Use json
+            config_dict = json.loads(file_handle)
 
     config_dict = copy.copy(DEFAULT_CONFIG)
-    config_dict.update(yaml_dict)
+    config_dict.update(config_dict)
 
     return config_dict
 
